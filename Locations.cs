64dc8752d147c144a7571746ede90b2ca6469b50
Rex Meyer <armeyer1998@gmail.com>
2021-10-12 20:48:14 -0400

    level four completed

--- Locations.cs
+++ Locations.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static PairProgrammingConsoleAssignment.ProgramUI;
 
 namespace PairProgrammingConsoleAssignment
 {
     class Locations
     {
-        public List<string> Exits { get; }
-        public string Filler { get;  }
 
-        public Locations(string filler, List<string> exits )
+        
+        //public List<string> Exits { get; }
+       // public string Filler { get;  }
+        public List<Item> Items { get; }
+        public void AddItem (Item item)
         {
-            Exits = exits;
+            if (Items.Contains(item))
+            {
+                Items.Add(item);
+            }
         }
+        public Locations() { }
+        public Locations(string filler, List<string> exits, List<Item> items )
+        {
+            //Exits = exits;
+            Items = items;
+        }
+
+       
     }
 }
--- ProgramUI.cs
+++ ProgramUI.cs
@@ -9,41 +9,41 @@ namespace PairProgrammingConsoleAssignment
     class ProgramUI
     {
         //items needed to win the game
-        //public enum Item { sword, shield };
-        //public List<Item> supplies = new List<Item>();
+        public enum Item { sword, shield };
+        public List<Item> supplies = new List<Item>();
         //list of rooms
-        Dictionary<string, Locations> Locations = new Dictionary<string, Locations>()
-        {
-            {"outside cell", outside_cell },
-            {"bedroom", bedroom },
-            {"dining room", dining_room },
-            {"office",  office },
-            {"main entrance",  main_entrance },
-            {"courtyard",  courtyard },
-        };
-        public static Locations bedroom = new Locations("You are in what looks to be a bedroom.\n" + "The bed is unmade, and the night stand has an abstract painting of what vaguely appears to be a dragon.\n" +
-            "On the other side of the bed room, a class case is holding an almighty sword.\n" +
-            "Do you want take the SWORD?\n",
-            new List<string> { "outside cell" });
-        public static Locations dining_room = new Locations("You are now in a elegant dining room where three doors\n" +
-            " stand at the end of the hallway.\n" +
-            "Each door has as sign above them stating their respective rooms.\n" +
-            "Which door would you like to go through?\n" +
-            "OFFICE, MAIN ENTRACE, or COURTYARD", new List<string> { "office", "main entrance", "courtyard" });
-        public static Locations outside_cell = new Locations("You see two doors.\n" +
-                    "The left door says BEDROOM and the right door says DINING ROOM.\n" + "Which room do you want to enter?\n",
-            new List<string> { "dining room", "bedroom" });
-        public static Locations office = new Locations("In what looks to be a dimly lit, yet sophisticated office\n" +
-            "there is SHIELD hanging on the wall above the fireplace.\n" +
-            "Do you want to take the SHIELD?", new List<string> { "dining room" });
-        public static Locations main_entrance = new Locations("A grand staircase stands before you, but you want leave.\n" +
-            "But on the right side of the MAIN ENTRANCE is a set of double doors.\n" +
-            "Finally, an exit!\n" +
-            "Unfortunately, when you grab the handles, the door does not budge.\n" +
-            "You are stuck inside and nowhere else to go besides the DINING ROOM.", new List<string> { "dining room" });
-        public static Locations courtyard = new Locations("AHHHHH!\n" +
-            "The fire breathing dragon stands before you.\n",
-            new List<string> { });
+        //Dictionary<string, Locations> Locations = new Dictionary<string, Locations>()
+        //{
+        //    {"outside cell", outside_cell },
+        //    {"bedroom", bedroom },
+        //    {"dining room", dining_room },
+        //    {"office",  office },
+        //    {"main entrance",  main_entrance },
+        //    {"courtyard",  courtyard },
+        //};
+        //public static Locations bedroom = new Locations("You are in what looks to be a bedroom.\n" + "The bed is unmade, and the night stand has an abstract painting of what vaguely appears to be a dragon.\n" +
+        //    "On the other side of the bed room, a class case is holding an almighty sword.\n" +
+        //    "Do you want take the SWORD?\n",
+        //    new List<string> { "outside cell" });
+        //public static Locations dining_room = new Locations("You are now in a elegant dining room where three doors\n" +
+        //    " stand at the end of the hallway.\n" +
+        //    "Each door has as sign above them stating their respective rooms.\n" +
+        //    "Which door would you like to go through?\n" +
+        //    "OFFICE, MAIN ENTRACE, or COURTYARD", new List<string> { "office", "main entrance", "courtyard" });
+        //public static Locations outside_cell = new Locations("You see two doors.\n" +
+        //            "The left door says BEDROOM and the right door says DINING ROOM.\n" + "Which room do you want to enter?\n",
+        //    new List<string> { "dining room", "bedroom" });
+        //public static Locations office = new Locations("In what looks to be a dimly lit, yet sophisticated office\n" +
+        //    "there is SHIELD hanging on the wall above the fireplace.\n" +
+        //    "Do you want to take the SHIELD?", new List<string> { "dining room" });
+        //public static Locations main_entrance = new Locations("A grand staircase stands before you, but you want leave.\n" +
+        //    "But on the right side of the MAIN ENTRANCE is a set of double doors.\n" +
+        //    "Finally, an exit!\n" +
+        //    "Unfortunately, when you grab the handles, the door does not budge.\n" +
+        //    "You are stuck inside and nowhere else to go besides the DINING ROOM.", new List<string> { "dining room" });
+        //public static Locations courtyard = new Locations("AHHHHH!\n" +
+        //    "The fire breathing dragon stands before you.\n",
+        //    new List<string> { });
         //main menu
         public void Title()
         {
@@ -96,7 +96,8 @@ namespace PairProgrammingConsoleAssignment
                 if (input2 == "BEDROOM")
                 {
                     Console.WriteLine("You are in what looks to be a bedroom.\n" + "The bed is unmade, and the night stand has an abstract painting of what vaguely appears to be a dragon.\n" + "Congratulations! You have found the almighty sword. \n" + "Press Enter to continue exit the room....");
-                    Console.ReadLine();
+                    supplies.Add(Item.sword);
+                    Console.ReadKey();
                     Console.Clear();
                 }
                 else if (input2 == "DINING ROOM")
@@ -122,7 +123,7 @@ namespace PairProgrammingConsoleAssignment
             while (insideDiningRoom)
             {
                 Console.WriteLine("You are in the dining room. There are three doors ahead of you. One says OFFICE, one says MAIN ENTRANCE, and one says COURTYARD. Which door do you choose?");
-            
+
                 string doorChoice = Console.ReadLine().ToUpper();
                 if (doorChoice == "OFFICE")
                 {
@@ -137,7 +138,9 @@ namespace PairProgrammingConsoleAssignment
                         string input3 = Console.ReadLine().ToUpper();
                         if (input3 == "YES")
                         {
+                            supplies.Add(Item.shield);
                             Console.WriteLine("You now have obtained the SHIELD. \n" + "Press Enter to continue.....");
+
                             Console.ReadLine();
                             Console.Clear();
                             break;
@@ -163,6 +166,7 @@ namespace PairProgrammingConsoleAssignment
                 }
                 else if (doorChoice == "COURTYARD")
                 {
+                    Console.WriteLine("The smell of smoke and fire permeates your senses. A foggy cloud blankets your vision. A sinister growl and heavy, earthshaking footsteps lead you forward...");
                     break;
                 }
                 else
@@ -174,6 +178,16 @@ namespace PairProgrammingConsoleAssignment
         }
         public void LevelFour()
         {
+            Console.WriteLine("The dragon is in front of you");
+            Console.ReadKey();
+            if (supplies.Contains(Item.sword) && supplies.Contains(Item.shield))
+            {
+                Console.WriteLine("You won!");
+            }
+            else
+            {
+                Console.WriteLine("You die!");
+            }
 
         }
     }
